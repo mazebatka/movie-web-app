98152f66b8d3ca50fb75d409c4bdecc56785ac9b
Batka Nergui <batka@macbookpro.mynetworksettings.com>
2025-04-23 11:33:00 -0500

    page.tsx

--- src/app/category/[...segments]/page.tsx
+++ src/app/category/[...segments]/page.tsx
@@ -1,15 +1,14 @@
 import { Category } from "@/components";
-import {Suspense} from "react";
+import { Suspense } from "react";
 
-const MovieCategory= async ({params,searchParams}:
-    PageRouteParams)=>{
-    const {segments}= await params;
-    const {page="1"}= await searchParams;
-    
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Category page={Number(page)} segments={segments} />
-        </Suspense>
-    );
-    };
-    export default MovieCategory;
+const MovieCategory = async ({ params, searchParams }: PageRouteParams) => {
+  const { segments } = await params;
+  const { page = "1" } = await searchParams;
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Category page={Number(page)} segments={segments} />
+    </Suspense>
+  );
+};
+export default MovieCategory;
